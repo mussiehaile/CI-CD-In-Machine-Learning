33bec697432b29a80e59b22ca981bfd3bd79862c
mussie haile <44857352+mussiehaile@users.noreply.github.com>
2023-07-02 23:34:53 +0300

    Update train.py

--- train.py
+++ train.py
@@ -24,7 +24,7 @@ y = data['quality']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Initialize the random forest regressor
-model = RandomForestRegressor(n_estimators=100, random_state=42)
+model = RandomForestRegressor(n_estimators=150, random_state=42)
 
 # Train the model
 model.fit(X_train, y_train)
